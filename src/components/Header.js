16e8afc95c3a67a673d0b2ff14bc26df10f3e9e9
quentindnorris <97061466+quentindnorris@users.noreply.github.com>
2022-03-20 21:51:25 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import styled from 'styled-components'
+
+const Header = () => {
+  return (
+    <Nav>
+      
+    </Nav>
+  )
+}
+
+export default Header
---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import Header from "./header"
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+      <Header/>
+        <main>{children}</main>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
